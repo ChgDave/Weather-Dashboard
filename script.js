ef97731f096434b6733d186e7f8ca74fe685ee7e
ChgDave <chgdave@gmail.com>
2023-08-26 17:23:45 -0500

    David added 5 day forecast

--- script.js
+++ script.js
@@ -7,20 +7,27 @@ const form = document.querySelector("#searchForm");
 const dispCurrent = document.querySelector("#currentForecast");
 const dispFiveDay = document.querySelector("#fiveDayForecast");
 
+// add function to render weather data
 function display(data) {
+  currentWeather(data);
+  fiveDayWeather(data);
+}
+
+// add function to render current weather
+function currentWeather(data) {
   const headerDisp = document.createElement("h2");
   const headerImg = document.createElement("img");
   const tempDisp = document.createElement("p");
   const windDisp = document.createElement("p");
   const humidityDisp = document.createElement("p");
 
-  headerDisp.textContent = `${data.city.name} (${dayjs().format(
-    "MM/DD/YYYY"
-  )})`;
+  headerDisp.textContent = `${data.city.name} (${dayjs
+    .unix(data.list[0].dt)
+    .format("MM/DD/YYYY")})`;
   headerImg.src = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}@2x.png`;
-  tempDisp.textContent = `Temp: ${data.list[0].main.temp}`;
-  windDisp.textContent = `Wind: ${data.list[0].wind.speed}`;
-  humidityDisp.textContent = `Humidity: ${data.list[0].main.humidity}`;
+  tempDisp.textContent = `Temp: ${data.list[0].main.temp}°F`;
+  windDisp.textContent = `Wind: ${data.list[0].wind.speed} MPH`;
+  humidityDisp.textContent = `Humidity: ${data.list[0].main.humidity} %`;
 
   headerDisp.classList.add("disp");
   tempDisp.classList.add("disp");
@@ -29,6 +36,43 @@ function display(data) {
   headerImg.classList.add("icon");
   dispCurrent.append(headerDisp, tempDisp, windDisp, humidityDisp);
   headerDisp.append(headerImg);
+}
+
+// add function to render 5 day forecast weather
+function fiveDayWeather(data) {
+  for (let i = 8; i < data.list.length; i += 8) {
+    console.log(i);
+    weatherCard(data, i);
+  }
+  weatherCard(data, 39);
+}
+
+// add function to create weather card
+function weatherCard(data, i) {
+  const card = document.createElement("div");
+  const headerDisp = document.createElement("h4");
+  const headerImg = document.createElement("img");
+  const tempDisp = document.createElement("p");
+  const windDisp = document.createElement("p");
+  const humidityDisp = document.createElement("p");
+
+  headerDisp.textContent = `${data.city.name} (${dayjs
+    .unix(data.list[i].dt)
+    .format("MM/DD/YYYY")})`;
+  headerImg.src = `https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}@2x.png`;
+  tempDisp.textContent = `Temp: ${data.list[i].main.temp}°F`;
+  windDisp.textContent = `Wind: ${data.list[i].wind.speed} MPH`;
+  humidityDisp.textContent = `Humidity: ${data.list[i].main.humidity} %`;
+
+  headerDisp.classList.add("disp");
+  tempDisp.classList.add("disp");
+  windDisp.classList.add("disp");
+  humidityDisp.classList.add("disp");
+  // headerImg.classList.add("icon");
+  card.classList.add("card");
+
+  card.append(headerDisp, headerImg, tempDisp, windDisp, humidityDisp);
+  dispFiveDay.append(card);
 }
 
 function getData(e) {
@@ -41,11 +85,9 @@ function getData(e) {
       return response.json();
     })
     .then(function (geoData) {
-      console.log(geoData);
       // construct the URL for the 5 day 3 hour forecast
       const urlForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${geoData[0].lat}&lon=${geoData[0].lon}&appid=7903dacd04b26691274b62eff226d9c4&units=imperial`;
 
-      console.log(urlForecast);
       // fetch 5 day 3 hour forecast
       fetch(urlForecast)
         .then(function (response) {
